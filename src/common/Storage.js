eaa00a0e5e254be50cc7642444346a7d4931f4d0
Nick Grealy <nickgrealy@gmail.com>
2021-02-22 09:38:28 +1100

    Fixed gatsby server side build issue.

--- src/common/Storage.js
+++ src/common/Storage.js
@@ -1,9 +1,17 @@
 
-export const save = (name, yamlString) => localStorage.setItem(name, yamlString)
+const internalGetStorage = () => {
+  if (typeof localStorage !== 'undefined') {
+    return localStorage
+  } else {
+    return {}
+  }
+}
 
-export const load = (name) => localStorage.getItem(name)
+export const save = (name, yamlString) => internalGetStorage().setItem(name, yamlString)
+
+export const load = (name) => internalGetStorage().getItem(name)
 
 /**
  * @returns {Array<String>} - a list of keys
  */
-export const list = () => Object.keys(localStorage)
+export const list = () => Object.keys(internalGetStorage())
